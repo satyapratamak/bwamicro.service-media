408b78e1395f3183ed34af52ceca58699cf1a973
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2022-06-10 17:18:44 +0700

    Service Media : GET API List Image and DELETE API Image DONE

--- routes/media.js
+++ routes/media.js
@@ -4,10 +4,32 @@ const {PORT} = process.env;
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 const {Media} = require('../models');
+const fs = require('fs'); // filesystem
+
+
+/** GET List Image **/
+//router.get('/',)
+router.get('/', async(req, res)=>{
+  const media = await Media.findAll({
+    attributes: ['id','image'],
+  });
+
+  const mappedMedia = media.map((m) =>{
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status : "success",
+    data : mappedMedia,
+  });
+});
+
 
 /* POST image listing. */
 router.post('/', function(req, res){
   const image = req.body.image;
+  
 
   if(!isBase64(image, {mimeRequired : true})){
     return res.status(400).json({status : 'Bad Request', message : 'Invalid base64'});
@@ -27,7 +49,7 @@ router.post('/', function(req, res){
         const filename = filepath.split("\\").pop().split("/").pop(); // split filepath with backslash character and get the last element  for Windows
         //const filename = filepath.split("/").pop(); // for UNIX platforms
 
-        const media = await Media.create({image:`image/${filename}`});
+        const media = await Media.create({image:`images/${filename}`});
 
         return res.json({
           status : 'success',
@@ -42,6 +64,41 @@ router.post('/', function(req, res){
     });
 
     // 
+  }
+});
+
+/** DELETE Image **/
+router.delete('/:id', async(req, res) => {
+  const id = req.params.id;
+  
+
+  const media = await Media.findByPk(id);
+
+  if(!media){
+    // Media not found
+    return res.status(404).json(
+      {status:"Error", message: 'Media not found'}
+    );
+  }else{
+    // Media found in database
+    // Delete from file system and from database
+
+    // Delete image from file systems
+    fs.unlink(`./public/${media.image}`, async (err) => {
+      if (err){
+        return res.status(400).json({ status : "Error", message : err.message
+        });
+      }
+      
+    });
+
+    await media.destroy();
+
+    return res.json({
+      status : "Success",
+      message : "Image has been deleted successfully",
+    });
+
   }
 });
 
